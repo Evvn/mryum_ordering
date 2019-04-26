cfd10911bf4d39b2f50847999b0cf03460ac34f5
davjones0 <davjones0@yahoo.com>
2019-04-25 22:01:49 -0400

    search engine logic

--- src/components/Common/MenuSearch.js
+++ src/components/Common/MenuSearch.js
@@ -6,28 +6,26 @@ class MenuSearch extends React.Component {
         this.result = [];
     }
 
-    // Sections: Drinks, Price: 4, Item Name: Coffee | Tea | Etc, Item Description: Batch Brew Filter 
     search = async input => {
         const {data} = this.props;
-        const _dataKeys = Object.keys(data);
+        const _dataKeys = Object.keys(data.menuByItem);
+        let big_bucket = [];
+
         if (input) {
             let expression = new RegExp(`${input}`, 'gi');
             
-            let big_bucket = [];
             
-            // refactor to run async
             let sectionsPromise = new Promise(resolve => {
                 let bucket = [];
                 for (const key of _dataKeys) {
-                    let fields = data[key].fields;
+                    let fields = data.menuByItem[key].fields;
                     if (expression.test(fields['category'])) {
-                        let fields = data[key].fields;
+                        let fields = data.menuByItem[key].fields;
                         let searchScore = this.score(fields['category'].toString(), expression);
                         bucket.push({result: fields['category'], score: searchScore});
                     }
                 }
                 
-                console.log('sections', bucket)
                 resolve(bucket);
             });
 
@@ -35,75 +33,78 @@ class MenuSearch extends React.Component {
                 let bucket = [];
                 let expression = new RegExp(`${input}`, 'gi');
                 for (const key of _dataKeys) {
-                    let fields = data[key].fields;
+                    let fields = data.menuByItem[key].fields;
                     
                     if (expression.test(fields['price'])) {
-                        let fields = data[key].fields;
-                        let searchScore = this.score(fields['price'].toString(), expression);
+                        let fields = data.menuByItem[key].fields;
+                        let searchScore = this.score(fields['price'].toString(), expression, 'price');
                         bucket.push({result: fields['price'], score: searchScore});
                     }
                 }
-                console.log('price', bucket)
+
                 resolve(bucket);
             });
 
             let itemNamePromise = new Promise(resolve => {
                 let bucket = [];
                 for (const key of _dataKeys) {
-                    let fields = data[key].fields;
+                    let fields = data.menuByItem[key].fields;
                 
                     if (expression.test(fields['name'])) {
-                        let fields = data[key].fields;
+                        let fields = data.menuByItem[key].fields;
                         let searchScore = this.score(fields['name'].toString(), expression);
                         bucket.push({result: fields['name'], score: searchScore});
                     }
                 }
 
-                console.log('item name', bucket)
                 resolve(bucket);
             });
 
             let itemDescriptionPromise = new Promise(resolve => {
                 let bucket = [];
                 for (const key of _dataKeys) {
-                    let fields = data[key].fields;
+                    let fields = data.menuByItem[key].fields;
                 
                     if (expression.test(fields['description'])) {
-                        let fields = data[key].fields;
+                        let fields = data.menuByItem[key].fields;
                         let searchScore = this.score(fields['description'].toString(), expression);
                         bucket.push({result: fields['description'], score: searchScore});
                     }
                 }
 
-                console.log('description', bucket)
                 resolve(bucket);
             });
             
             
 
-            const [sections, price, itemName, itemDesc] = await Promise.all([sectionsPromise, pricePromise, itemNamePromise, itemDescriptionPromise]);
+            let [sections, price, itemName, itemDesc] = await Promise.all([sectionsPromise, pricePromise, itemNamePromise, itemDescriptionPromise]);
 
             
-        
-            // expression.sort((a,b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0));
-            // let actionables = [];
-            // bucket.map(x => actionables.push(x.result));
-            // this.result = actionables;
-            //console.log(sections);
-
+            big_bucket = big_bucket.concat(sections, price, itemName, itemDesc);
+            big_bucket.sort((a,b) => (a.score < b.score) ? 1 : ((b.score < a.score) ? -1 : 0));
+            
+            let actionables = [];
+            big_bucket.map(x => actionables.push(x.result));
+            return actionables;
         }
     }
 
 
-    // set some kind of weight on . or , for price
     score = (entry, regex, field) => {
         let matched = entry.match(regex);
         if (matched) {
             let result;
             switch (field) {
                 case 'price':
-                    
-                    result = matched.length / entry.length;
+                    let weight = 0;
+                    // weight decimal places and commas(maybe?) higher for prices
+                    for (let char of entry) {
+                        if (char === '.' || char === ',') {
+                            weight += .20;
+                        }
+                    }
+
+                    result = (matched.length / entry.length) + weight;
                     break;
             
                 default:
